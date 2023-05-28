35d0e5bd385e48f7a5d166ec01107f84c6dd0c5c
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2023-05-28 16:08:08 -0400

    dictionary and tuple classwork

---
+++ Basics/Tuple.py
@@ -0,0 +1,9 @@
+# Tuple declaration
+fruitTuple = ("Apple", "Orange", "Banana", "Plum", "Banana")
+print("Items in Tuple {} ".format(fruitTuple))
+
+print("Type of object {}".format(type(fruitTuple)))
+
+print("Banana repetition time : {}".format(fruitTuple.count("Banana")))
+
+print("Index of Orange {}".format(fruitTuple.index("Orange")))
