29c000526dc78efa16ae7a1f07743af3e8624c49
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2023-05-29 01:07:57 -0400

    read file write and module import

--- Basics/WriteFile.py
+++
@@ -1,6 +0,0 @@
-# writing overrides the data in file present earlier and starts writing from 1st line
-# appending adds the text from the line already present in the file
-file_to_write = open("WriteFile.txt", "w")
-for i in range(1, 2):
-    file_to_write.write("\nHello! I am writing text.")
-file_to_write.close()
