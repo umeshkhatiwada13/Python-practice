793ff444a148d1c623cdf40bb1a22da11544f16d
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2023-05-30 20:33:10 -0400

    learn python class and its implementation

---
+++ Basics/Classtask/FootballerClass.py
@@ -0,0 +1,7 @@
+class Footballer:
+    def __init__(self, name, age, country, position, is_starter):
+        self.name = name
+        self.age = age
+        self.country = country
+        self.position = position
+        self.is_starter = is_starter
---
+++ Basics/Classtask/RealMadridClassImpl.py
@@ -0,0 +1,7 @@
+from FootballerClass import Footballer
+
+ronaldo = Footballer("C. Ronaldo", 38, "Portugal", "Striker", True)
+ramos = Footballer("Ramos", 36, "Portugal", "Defender", True)
+
+print("Ramos plays as a : {}".format(ramos.position))
+print("Age of Ronaldo is : {}".format(ronaldo.age))
