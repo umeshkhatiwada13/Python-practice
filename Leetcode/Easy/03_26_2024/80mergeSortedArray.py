d1e0e8877c1a1896fbebead40148c56abd065a57
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2024-03-27 00:30:38 -0400

    feat: working on merge sorted array easy

---
+++ Leetcode/Easy/03_26_2024/80mergeSortedArray.py
@@ -0,0 +1,17 @@
+def merge_sorted_array(a, b):
+    temp = int(-2 ** 32)
+    j = 0
+    for i in range(0, len(a)):
+        if (temp > a[i]):
+            a[i] = temp
+            temp = int(-2 ** 32)
+        elif (a[i] > b[j]):
+            temp = a[i]
+            a[i] = b[j]
+            j = j + 1
+        elif (a[i] < b[j]):
+            continue
+    return a
+
+
+print(merge_sorted_array([1, 2, 3, 0, 0, 0], [2, 5, 6]))
